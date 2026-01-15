55628963a9ecc888b12542476afd3088800f6336
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2026-01-15 18:01:46 +0000

    config changes done for testing purpose

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from 'lovable-tagger';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/praana-test/',
+  base: mode === 'production' ? '/praana-test/' : '/',
   build: {
     outDir: 'docs',
   },
