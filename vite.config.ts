3b65bab1d1fc90a7a520669933111c4a64602b38
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2026-01-15 17:32:18 +0000

    vita config done

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { componentTagger } from 'lovable-tagger';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: mode === 'production' ? '/praana-test/' : '/',
+  build: {
+    outDir: 'docs',
+  },
   server: {
     host: '::',
     port: 8080,
